362b33ceba611639ca69c99807aa201398d83927
Ayesha Aziz <163914368+ayesha-aziz123@users.noreply.github.com>
2024-09-21 15:22:05 +0500

    Delete TIC_TAC_TOE2 directory

--- TIC_TAC_TOE2/main.js
+++
@@ -1,6 +0,0 @@
-let boxes = document.querySelector(".box")
-let resetbtn = document.querySelector("#reset")
-let newbtn = document.querySelector("#new-btn")
-let msgContainer = document.querySelector(".msg-container")
-let msg = document.querySelector("#msg")
-
